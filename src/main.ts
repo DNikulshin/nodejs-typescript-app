1206dc94891268d64aa70d5328c56e1990e60438
Dmitriy Nikulshin <98643483+DNikulshin@users.noreply.github.com>
2024-05-10 19:08:07 +0000

    init

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { analytics } from "./modules/analytics.js";
-const message: string = "Hello NodeJS";
-console.log(message);
-analytics("Main.ts!!!!!!!");
+import { analytics } from './modules/analytics.js'
+const message: string = 'Hello NodeJS'
+console.log(message)
+analytics('Main.ts!!!!!!!')
