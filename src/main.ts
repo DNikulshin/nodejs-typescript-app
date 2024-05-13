0f1f661559ad0e2194ff346528dad195c5ba72e1
dmnitprof <dmn.itprof@gmail.com>
2024-05-13 23:33:17 +0300

    edit package.json

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
 import express from 'express'
+import path from 'path'
 import cors from 'cors'
 import { sequelize } from './db/connect.js'
 import routes from './router/task.rourer.js'
-const message: string = 'Hello NodeJS'
+const __dirname = path.resolve()
 
 const app = express()
 app.use(cors())
@@ -11,19 +12,26 @@ app.use(express.urlencoded({ extended: false }))
 
 app.use(routes)
 
+console.log('process.env.ENV_DEV', process.env.ENV_DEV)
+console.log('process.env.ENV_PROD', process.env.ENV_PROD)
+
+if (process.env.ENV_PROD === 'production') {
+  app.use('/', express.static(path.join(__dirname, 'client', 'dist')))
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'dist', 'index.html'))
+  })
+}
+
 async function start() {
   try {
     await sequelize.authenticate()
     await sequelize.sync()
-    app.get('/', (req, res) => {
-      res.send(message)
-    })
-
     app.listen(3000, () => {
       console.log('Server is running on port 3000')
     })
   } catch (error) {
     console.error(error)
+    process.exit(1)
   }
 }
 start()
