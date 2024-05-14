90acf83eeb346a453cec9ee96daefcbd37a9b48d
dmnitprof <dmn.itprof@gmail.com>
2024-05-14 07:15:08 +0300

    add methods

--- src/controllers/task.controller.ts
+++ src/controllers/task.controller.ts
@@ -1,52 +1,66 @@
 import { Request, Response } from 'express'
 import { Task } from '../models/Task.js'
-import mockData from '../db/mockdata.js'
-
-// const testData = [
-//   {
-//     title: 'Task 1',
-//     description: 'Description 1',
-//   },
-//   {
-//     title: 'Task 2',
-//     description: 'Description 2',
-//   },
-// ]
 
 export const getTasks = async (req: Request, res: Response) => {
   try {
-    console.log(req.body)
     const tasks = await Task.findAll()
-    console.log('tasks db getAll', tasks)
-    // console.log('testData', testData)
+    if(!tasks) {
+      res.status(404).json({ message: 'Tasks not found...' })
+    }
     res.status(200).json(tasks)
+  } catch (error) {
+    res.status(500).json({ message: error })
+  }
+}
+
+export const getTask = async (req: Request, res: Response) => {
+  try {
+    const {id} = req.params
+    if(!id) {
+      res.status(404).json({message: `Not id: ${id}`})
+    }
+    const task = await Task.findOne({
+      where: {id}
+    })
+    if(!task) {
+      res.status(404).json({ message: `Task id: ${id} not found...` })
+    }
+    res.status(200).json(task)
   } catch (error) {
     res.status(500).json({ message: error })
   }
 }
 export const createTask = async (req: Request, res: Response) => {
   try {
-    console.log(req.body)
-    // testData.push({
-    //   title: req.body.title,
-    //   description: req.body.description,
-    // })
+    if(req.body.title || req.body.description) {
+      res.status(400).json({message: 'Bad Request not title || description'})
+    }
 
     const tasks = await Task.create({
       title: req.body.title,
       description: req.body.description,
     })
-    // const test = mockData.createItem({
-    //   title: req.body.title,
-    //   description: req.body.description,
-    // })
 
-    // const data = await mockData.getAll()
-
-    // console.log('test', test)
-    // console.log('testData', testData)
-    console.log('tasks db create', tasks)
     res.status(201).json(tasks)
+  } catch (error) {
+    res.status(500).json({ message: error })
+  }
+}
+
+export const removeTask = async (req: Request, res: Response) => {
+  try {
+    const {id} = req.params
+    if(!id) {
+      res.status(404).json({message: `Not id: ${id}`})
+    }
+
+    const task = await Task.destroy({
+      where: {id}
+    })
+    if(!task) {
+      res.status(404).json({message: `task id: ${id} not found`})
+    }
+    res.status(204).json(`remove task id: ${id}`)
   } catch (error) {
     res.status(500).json({ message: error })
   }
--- src/router/task.rourer.ts
+++ src/router/task.rourer.ts
@@ -1,8 +1,11 @@
 import { Router } from 'express'
-import { getTasks, createTask } from '../controllers/task.controller.js'
+import {getTasks, createTask, removeTask, getTask} from '../controllers/task.controller.js'
 const router = Router()
 
 router.get('/tasks', getTasks)
+router.get('/tasks/:id', getTask)
 router.post('/tasks', createTask)
+router.delete('/tasks/:id', removeTask)
+
 
 export default router
