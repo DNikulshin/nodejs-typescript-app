5b7188f8b7cc75ac502b29e92973b429b13a0719
dmnitprof <dmn.itprof@gmail.com>
2024-09-24 18:23:51 +0300

    add error handler, tostify

--- client/src/components/LoginForm.tsx
+++ client/src/components/LoginForm.tsx
@@ -5,18 +5,28 @@ import { useStore } from "../store/store"
 export const LoginForm: FC = () => {
     const [email, setEmail] = useState<string>('')
     const [password, setPassword] = useState<string>('')
-    const isAuth = useStore(state => state.isAuth)
+    const isLoading = useStore(state => state.isLoading)
+
     const login = useStore(state => state.login)
 
     const registration = useStore(state => state.registration)
 
+  if(isLoading) {
     return (
-        <div className="flex flex-col w-full px-4 justify-center items-center gap-4">
-            <h1 className="text-3xl">{isAuth ? 'registrationForm ' : ' LoginForm' }</h1>
+      <div>Loading...</div>
+    )
+  }
+
+    return (
+        <form className="flex flex-col w-full px-4 justify-center items-center gap-4">
+            <h1 className="text-3xl">Auth Form</h1>
 
             <input
                 className="bg-gray-200 mb-2 px-2 py-2 shadow-md"
-                onChange={e => setEmail(e.target.value)}
+                onChange={e => {
+                    setEmail(e.target.value)
+
+                }}
                 value={email}
                 type="text"
                 placeholder="Email"
@@ -24,20 +34,20 @@ export const LoginForm: FC = () => {
 
             <input
                 className=" bg-gray-200 px-2 py-2 shadow-md"
-                onChange={e => setPassword(e.target.value)}
+                onChange={e => {
+
+                    setPassword(e.target.value)
+                  }
+                }
                 value={password}
                 type="password"
                 placeholder="Пароль"
             />
 
-            {isAuth ? <button className="px-2 py-2 bg-gray-400 shadow-md" onClick={() => login(email, password)}>Авторизация</button>
-           
-                :  <button className="px-2 py-2 bg-gray-400 shadow-md" onClick={() => registration(email, password)}>Регистрация</button>
-            }
+            <button disabled={isLoading} type="button" className="px-2 py-2 bg-gray-400 shadow-md" onClick={() => login(email, password)}>Авторизация</button>
 
+               <button disabled={isLoading}  type="button" className="px-2 py-2 bg-gray-400 shadow-md" onClick={() => registration(email, password)}>Регистрация</button>
 
-
-
-        </div>
+        </form>
     )
 }
--- client/src/store/store.ts
+++ client/src/store/store.ts
@@ -1,7 +1,9 @@
 
+import { AxiosError } from 'axios'
 import { create } from 'zustand'
 import { IUser } from '../models/User'
 import AuthService from '../services/AuthService'
+import { toast} from 'react-toastify'
 
 interface IStore {
     user: IUser
@@ -9,6 +11,16 @@ interface IStore {
     login: (email: string, password: string) => Promise<void>,
     registration: (email: string, password: string) => Promise<void>,
     logout: () => Promise<void>
+    isLoading: boolean
+    errors: IError[]
+}
+
+interface IError {
+    msg: string
+    path?: string
+    type?: string
+    value?: string
+
 }
 
 
@@ -19,43 +31,80 @@ export const useStore = create<IStore>()((set) => ({
         isActivated: false
     },
     isAuth: false,
+    isLoading: false,
+    errors: [],
 
     login: async (email, password) => {
         try {
+            set({errors: []})
+            set({isLoading: true})
             const response = await AuthService.login(email, password)
             localStorage.setItem('accessToken', response.data.accessToken)
             set({ isAuth: true })
             set({ user: response.data.user })
+            set({isLoading: false})
+        } catch (e) {
+            if(e instanceof AxiosError) {
+                toast(e.response?.data?.message, {type: 'error'})
+                set({errors: e.response?.data?.errors })
+                console.log(e.response?.data?.message)
+                console.log(e.response?.data?.errors)
+            } else {
+                toast(e.message, {type: 'error'})
+                set({errors: e?.errors })
+                console.log(e?.errors)
+                throw e
+            }
 
-        } catch (e: any) { // any ???
-            console.log(e)
-            console.log(e.response?.data?.message)
-
+        } finally {
+            set({isLoading: false})
         }
     },
     registration: async (email, password) => {
         try {
+            set({errors: []})
+            set({isLoading: true})
             const response = await AuthService.registration(email, password)
             localStorage.setItem('accessToken', response.data.accessToken)
             set({ isAuth: true })
             set({ user: response.data.user })
+            set({isLoading: false})
+        } catch (e) {
+            if(e instanceof AxiosError) {
+                toast(e.response?.data?.message, {type: 'error'})
+                set({errors: e.response?.data?.errors })
+                console.log(e.response?.data?.message)
+                console.log(e.response?.data?.errors)
+            } else {
+                toast(e.message, {type: 'error'})
+                set({errors: e?.errors })
+                console.log(e?.errors)
+                throw e
+            }
 
-        } catch (e: any) { // any ???
-            console.log(e)
-            console.log(e.response?.data?.message)
 
+        } finally {
+            set({isLoading: false})
         }
     },
     logout: async () => {
         try {
+            set({isLoading: true})
             await AuthService.logout()
             localStorage.removeItem('accessToken')
             set({ isAuth: false })
             set({ user: {} as IUser })
-
-        } catch (e: any) { // any ???
-            console.log(e.response?.data?.message)
-
+            set({isLoading: false})
+        } catch (e) { // any ???
+            if(e instanceof AxiosError) {
+                console.log(e)
+                console.log(e.response?.data?.message)
+                toast(e.response?.data?.message, {type: 'error'})
+            }
+            toast(e.message, {type: 'error'})
+            throw e
+        } finally {
+            set({isLoading: false})
         }
     }
 }))
