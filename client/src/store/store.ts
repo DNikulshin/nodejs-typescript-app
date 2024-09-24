3983567afcda814d2ed1f613a356f4b68b05b55f
d.nikulshin.dev <d.nikulshin.dev@gmail.com>
2024-09-24 18:19:06 +0000

    add error handler client

--- client/src/store/store.ts
+++ client/src/store/store.ts
@@ -1,9 +1,8 @@
 
-import { AxiosError } from 'axios'
 import { create } from 'zustand'
 import { IUser } from '../models/User'
 import AuthService from '../services/AuthService'
-import { toast} from 'react-toastify'
+import ErrorHandler from '../exeptions/ErrorHandler'
 
 interface IStore {
     user: IUser
@@ -12,16 +11,16 @@ interface IStore {
     registration: (email: string, password: string) => Promise<void>,
     logout: () => Promise<void>
     isLoading: boolean
-    errors: IError[]
+    errors: any[] //any type ???
 }
 
-interface IError {
-    msg: string
-    path?: string
-    type?: string
-    value?: string
+// interface IError {
+//     msg: string
+//     path?: string
+//     type?: string
+//     value?: string
 
-}
+// }
 
 
 export const useStore = create<IStore>()((set) => ({
@@ -36,75 +35,50 @@ export const useStore = create<IStore>()((set) => ({
 
     login: async (email, password) => {
         try {
-            set({errors: []})
-            set({isLoading: true})
+            set({ errors: [] })
+            set({ isLoading: true })
             const response = await AuthService.login(email, password)
             localStorage.setItem('accessToken', response.data.accessToken)
             set({ isAuth: true })
             set({ user: response.data.user })
-            set({isLoading: false})
-        } catch (e) {
-            if(e instanceof AxiosError) {
-                toast(e.response?.data?.message, {type: 'error'})
-                set({errors: e.response?.data?.errors })
-                console.log(e.response?.data?.message)
-                console.log(e.response?.data?.errors)
-            } else {
-                toast(e.message, {type: 'error'})
-                set({errors: e?.errors })
-                console.log(e?.errors)
-                throw e
-            }
+            set({ isLoading: false })
+        } catch (error) {
+            return ErrorHandler(error)
 
         } finally {
-            set({isLoading: false})
+            set({ isLoading: false })
         }
     },
     registration: async (email, password) => {
         try {
-            set({errors: []})
-            set({isLoading: true})
+            set({ errors: [] })
+            set({ isLoading: true })
             const response = await AuthService.registration(email, password)
             localStorage.setItem('accessToken', response.data.accessToken)
             set({ isAuth: true })
             set({ user: response.data.user })
-            set({isLoading: false})
-        } catch (e) {
-            if(e instanceof AxiosError) {
-                toast(e.response?.data?.message, {type: 'error'})
-                set({errors: e.response?.data?.errors })
-                console.log(e.response?.data?.message)
-                console.log(e.response?.data?.errors)
-            } else {
-                toast(e.message, {type: 'error'})
-                set({errors: e?.errors })
-                console.log(e?.errors)
-                throw e
-            }
+            set({ isLoading: false })
+        } catch (error) {
+            return ErrorHandler(error)
 
 
         } finally {
-            set({isLoading: false})
+            set({ isLoading: false })
         }
     },
     logout: async () => {
         try {
-            set({isLoading: true})
+            set({ isLoading: true })
             await AuthService.logout()
             localStorage.removeItem('accessToken')
             set({ isAuth: false })
             set({ user: {} as IUser })
-            set({isLoading: false})
-        } catch (e) { // any ???
-            if(e instanceof AxiosError) {
-                console.log(e)
-                console.log(e.response?.data?.message)
-                toast(e.response?.data?.message, {type: 'error'})
-            }
-            toast(e.message, {type: 'error'})
-            throw e
+            set({ isLoading: false })
+        } catch (error) {
+           return ErrorHandler(error)
+
         } finally {
-            set({isLoading: false})
+            set({ isLoading: false })
         }
     }
 }))
