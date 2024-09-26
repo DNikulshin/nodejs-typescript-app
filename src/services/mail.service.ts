5ca5e6087b30cfc0323bf81d5ede0a9ea42717d0
dmnitprof <dmn.itprof@gmail.com>
2024-09-27 00:50:43 +0300

    add in store getUsers

--- src/services/mail.service.ts
+++ src/services/mail.service.ts
@@ -20,17 +20,17 @@ class MailService {
         console.log(to, link)
 
         await this.transporter.sendMail({
-            // from: process.env.SMTP_USER,
-            // to,
-            // subject: `Активация аккаунта на ${process.env.API_URL}`,
-            // text: '',
-            // html: `
-            //     <div>
-            //     <h1>Для активации перейдите по ссылке</h1>
-            //     <a href="${link}">${link}</a>
-            //     </div>
-            //
-            // `
+            from: process.env.SMTP_USER,
+            to,
+            subject: `Активация аккаунта на ${process.env.API_URL}`,
+            text: '',
+            html: `
+                <div>
+                <h1>Для активации перейдите по ссылке</h1>
+                <a href="${link}">${link}</a>
+                </div>
+
+            `
         })
     }
 }
